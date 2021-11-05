b728d9f259c7e9ea92b9d2348658e51fdda38d72
Joan <Joan@DESKTOP-PRDGS70>
2021-11-05 17:35:06 +0100

    added useragent to download in glitch

--- JoanClient/FoldersManager/Create.cs
+++ JoanClient/FoldersManager/Create.cs
@@ -34,6 +34,7 @@ namespace JoanpixerClient.FoldersManager
             if (!File.Exists(Environment.CurrentDirectory + "\\Joanpixer\\sound.wav"))
             {
                 var client = new System.Net.WebClient();
+                client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
                 MelonLogger.Msg("Downloading sound.wav");
                 client.DownloadFile(new Uri("https://joanpixertest.glitch.me/SDK/sound.wav"), "Joanpixer\\sound.wav");
             }
